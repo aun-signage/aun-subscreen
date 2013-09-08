5fac4ecc7118d5043c1da4a26461343126b306fa
Yoji Shidara <dara@shidara.net>
2013-09-08 13:18:10 +0900

    Setup Socket.IO

--- app.js
+++ app.js
@@ -1,9 +1,22 @@
 var express = require('express');
 var app = express();
+var server = require('http').createServer(app);
+var io = require('socket.io').listen(server);
+
 var port = process.env.PORT || 3000;
 
-app.get('/', function(req, res){
-    res.send('hello world');
+server.listen(port, function() {
+  console.log('aun-subscreen now listening on ' + port);
 });
 
-app.listen(port);
+app.get('/', function (req, res) {
+  res.sendfile(__dirname + '/public/index.html');
+});
+
+io.sockets.on('connection', function (socket) {
+  console.log('[' + socket.id + '] connected');
+
+  socket.on('disconnect', function() {
+    console.log('[' + socket.id + '] disconnected');
+  });
+});
