85010a303e3c3278bef9829ef27194770b9e3dc2
Yoji Shidara <dara@shidara.net>
2013-09-14 14:33:18 +0900

    Select on social stream update

--- routes/timeline/index.js
+++ routes/timeline/index.js
@@ -2,17 +2,22 @@ var express = require('express');
 
 module.exports = function(env, pgClient, socialStream) {
   var app = express();
+  var limit = 20;
 
   socialStream.on('update', function(data) {
-    var payload = data.payload;
-    console.log(
-      '[' + payload.user.screen_name + '] ' +
-      payload.text.replace(/\s+/g, ' ')
+    pgClient.query(
+      'SELECT * FROM messages ORDER BY time DESC LIMIT $1',
+      [limit],
+      function(err, result) {
+        if (err) {
+          throw 'Error in selecting ' + err;
+        }
+        result.rows.forEach(function(data) {
+          var payload = data.payload;
+          console.log('[' + payload.user.screen_name + '] ' + payload.text);
+        });
+      }
     );
-    pgClient.query('SELECT COUNT(*) FROM messages', function(err, result) {
-      var numMessages = result.rows[0].count;
-      console.log('#messages: %s', numMessages);
-    });
   });
 
   app.get('/', function(req, res) {
