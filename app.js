93d83ce756b4c3afab563b53cc781cc3eaaf62a0
Yoji Shidara <dara@shidara.net>
2013-09-14 10:14:51 +0900

    Use pg and store data into PostgreSQL

--- app.js
+++ app.js
@@ -2,8 +2,17 @@ var express = require('express');
 var app = express();
 var server = require('http').createServer(app);
 var io = require('socket.io').listen(server);
+var pg = require('pg');
 
 var port = process.env.PORT || 3000;
+var databaseUrl = process.env.DATABASE_URL;
+
+var pgClient = new pg.Client(databaseUrl);
+pgClient.connect(function(err) {
+  if (err) {
+    return console.error('failed to connect postgres', err);
+  }
+});
 
 server.listen(port, function() {
   console.log('aun-subscreen now listening on ' + port);
@@ -28,4 +37,13 @@ var tw = new Twitter(
 );
 tw.on('tweet', function(data) {
   console.log('[' + data.user.screen_name + '] ' + data.text.replace(/\s+/, ' '));
+  pgClient.query(
+    'INSERT INTO messages (type, time, payload) VALUES ($1, $2, $3)',
+    ['tweet', data.created_at, data],
+    function(err, result) {
+      if (err) {
+        throw 'Error in INSERT' + err;
+      }
+    }
+  );
 });
