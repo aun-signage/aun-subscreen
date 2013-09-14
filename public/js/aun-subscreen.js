8fd111d65d3f19007e3e13c25ca833e4a3ee6dc6
Yoji Shidara <dara@shidara.net>
2013-09-14 16:17:40 +0900

    Add jQuery

--- public/js/aun-subscreen.js
+++ public/js/aun-subscreen.js
@@ -1,5 +1,21 @@
-var socket = io.connect();
+jQuery(document).ready(function($) {
+  var socket = io.connect();
 
-socket.on('message', function(data) {
-  console.log(data);
+  var ViewModel = function(socket) {
+    var self = this;
+    var mapping = {
+      messages: {
+        key: function(data) {
+          return ko.utils.unwrapObservable(data._id);
+        }
+      }
+    };
+
+    self.messages = ko.observableArray();
+
+    mySocket.on('message', function(data) {
+      console.log(data);
+      ko.mapping.fromJS(data, mapping, self);
+    });
+  };
 });
