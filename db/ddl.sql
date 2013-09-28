9f94c0b162724234be0e957beae324bd3d3708a5
Yoji Shidara <dara@shidara.net>
2013-09-28 19:32:19 +0900

    Index on text of messages

--- db/ddl.sql
+++ db/ddl.sql
@@ -16,3 +16,8 @@ CREATE INDEX messages_channel ON messages ((payload ->> 'to')) WHERE type = 'irc
 DROP INDEX IF EXISTS messages_screen_name;
 CREATE INDEX messages_screen_name ON messages ((payload -> 'user' ->> 'screen_name')) WHERE type = 'tweet';
 
+
+DROP INDEX IF EXISTS messages_text;
+DROP EXTENSION IF EXISTS pg_trgm;
+CREATE EXTENSION pg_trgm;
+CREATE INDEX messages_text ON messages USING gin (text gin_trgm_ops) WHERE type = 'tweet';
