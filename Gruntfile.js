ad85b1bf133b9723e46bc73fb2c63a9dae046375
Yoji Shidara <dara@shidara.net>
2013-09-30 17:12:37 +0900

    Use grunt to compile less

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,6 +1,19 @@
 module.exports = function(grunt) {
   grunt.initConfig({
+    less: {
+      production: {
+        options: {
+          paths: ['assets/css'],
+          compress: true
+        },
+        files: {
+          'public/css/application.css': 'assets/css/application.less'
+        }
+      }
+    }
   });
 
-  grunt.registerTask('default', []);
+  grunt.loadNpmTasks('grunt-contrib-less');
+
+  grunt.registerTask('default', ['less:production']);
 };
