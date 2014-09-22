0a1d67e2b4b5067d7084c53f94bac5516a36eaf9
Yoji Shidara <dara@shidara.net>
2014-09-22 16:27:37 +0900

    Auto reconnect

--- public/js/app.js
+++ public/js/app.js
@@ -1,13 +1,26 @@
-var sock = new SockJS('/timeline');
-sock.onopen = function() {
-  console.log('open');
+var sock = null;
+
+var connect = function() {
+  if (sock) {
+    return;
+  }
+
+  sock = new SockJS('/timeline');
+
+  sock.onopen = function() {
+    console.log('open');
+  };
+
+  sock.onmessage = function(e) {
+    var data = JSON.parse(e.data);
+    console.log('message', data);
+  };
+
+  sock.onclose = function() {
+    console.log('close');
+    sock = null;
+  };
 };
 
-sock.onmessage = function(e) {
-  var data = JSON.parse(e.data);
-  console.log('message', data);
-};
-
-sock.onclose = function() {
-  console.log('close');
-};
+connect();
+setInterval(connect, 2000);
