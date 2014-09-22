9f0fc3c5a9f6f724a5c75ea4b000070d6d678216
Yoji Shidara <dara@shidara.net>
2014-09-22 22:51:31 +0900

    Introduce Twitter Bootstrap

--- client/gulpfile.js
+++ client/gulpfile.js
@@ -10,6 +10,9 @@ var paths = {
   vendorScripts: [
     "bower_components/react/react.js",
     "bower_components/sockjs/sockjs.min.js"
+  ],
+  vendorStylesheets: [
+    "bower_components/twitter/dist/css/bootstrap.min.css"
   ]
 };
 
@@ -26,11 +29,18 @@ gulp.task('vendor-scripts', function() {
     .pipe(gulp.dest('../public/js'));
 });
 
+gulp.task('vendor-stylesheets', function() {
+  return gulp.src(paths.vendorStylesheets)
+    .pipe(concat('vendor.css'))
+    .pipe(gulp.dest('../public/css'));
+});
+
 gulp.task('watch', function() {
   gulp.watch(paths.scripts, ['scripts']);
   gulp.watch(paths.vendorScripts, ['vendor-scripts']);
+  gulp.watch(paths.vendorStylesheets, ['vendor-stylesheets']);
 });
 
-gulp.task('build', ['scripts', 'vendor-scripts']);
+gulp.task('build', ['scripts', 'vendor-scripts', 'vendor-stylesheets']);
 
 gulp.task('default', ['watch']);
