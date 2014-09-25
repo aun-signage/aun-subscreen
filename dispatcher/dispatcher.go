4bc136f0991253d9d17b2bab16c41c180247e008
Yoji Shidara <dara@shidara.net>
2014-09-25 22:33:25 +0900

    Rename Subscriptions to ChannelsForQuery

--- dispatcher/dispatcher.go
+++ dispatcher/dispatcher.go
@@ -11,24 +11,24 @@ import (
 // TODO normalize queries to be more efficient
 
 type Dispatcher struct {
-	DB              *sql.DB
-	Subscriptions   map[string]map[chan []byte]struct{}
-	QueryForChannel map[chan []byte]string
-	ChannelsMutex   sync.Mutex
+	DB               *sql.DB
+	ChannelsForQuery map[string]map[chan []byte]struct{}
+	QueryForChannel  map[chan []byte]string
+	ChannelsMutex    sync.Mutex
 }
 
 func NewDispatcher(db *sql.DB) *Dispatcher {
 	subscriptions := make(map[string]map[chan []byte]struct{})
 	queries := make(map[chan []byte]string)
 	return &Dispatcher{
-		DB:              db,
-		Subscriptions:   subscriptions,
-		QueryForChannel: queries,
+		DB:               db,
+		ChannelsForQuery: subscriptions,
+		QueryForChannel:  queries,
 	}
 }
 
 func (d *Dispatcher) Dispatch() error {
-	for query, channels := range d.Subscriptions {
+	for query, channels := range d.ChannelsForQuery {
 		buf, err := timeline.Timeline(d.DB, query)
 		if err != nil {
 			return err
@@ -60,15 +60,15 @@ func (d *Dispatcher) Subscribe(ch chan []byte, query string) {
 	d.ChannelsMutex.Lock()
 	defer d.ChannelsMutex.Unlock()
 
-	if subscription, ok := d.Subscriptions[query]; ok {
+	if subscription, ok := d.ChannelsForQuery[query]; ok {
 		subscription[ch] = struct{}{}
 	} else {
-		d.Subscriptions[query] = make(map[chan []byte]struct{})
-		d.Subscriptions[query][ch] = struct{}{}
+		d.ChannelsForQuery[query] = make(map[chan []byte]struct{})
+		d.ChannelsForQuery[query][ch] = struct{}{}
 	}
 	d.QueryForChannel[ch] = query
 
-	log.Printf("SUBSCRIBE: Channels: %d, Connections: %d", len(d.Subscriptions), len(d.QueryForChannel))
+	log.Printf("SUBSCRIBE: Queries: %d, Connections: %d", len(d.ChannelsForQuery), len(d.QueryForChannel))
 }
 
 func (d *Dispatcher) Unsubscribe(ch chan []byte) {
@@ -80,15 +80,15 @@ func (d *Dispatcher) Unsubscribe(ch chan []byte) {
 		return
 	}
 
-	subscription, ok := d.Subscriptions[query]
+	subscription, ok := d.ChannelsForQuery[query]
 	if !ok {
 		return
 	}
 	delete(d.QueryForChannel, ch)
 	delete(subscription, ch)
 	if len(subscription) == 0 {
-		delete(d.Subscriptions, query)
+		delete(d.ChannelsForQuery, query)
 	}
 
-	log.Printf("UNSUBSCRIBE: Channels: %d, Connections: %d", len(d.Subscriptions), len(d.QueryForChannel))
+	log.Printf("UNSUBSCRIBE: Queries: %d, Connections: %d", len(d.ChannelsForQuery), len(d.QueryForChannel))
 }
